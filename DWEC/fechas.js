c2cf3d2d921ad83fea07cb2a6866dc8032c01e5c
adrialba <adrian.alba@outlook.es>
2022-10-17 20:01:56 +0200

    PHP

--- DWEC/fechas.js
+++ DWEC/fechas.js
@@ -1,9 +1,12 @@
 let fecha = new Date();
+//Indica la fecha del día de hoy
 let dia = ["Domingo", "Lunes", "Martes", "Mi&eacute;rcoles", "Jueves", "Viernes", "S&aacute;bado"];
 let mes = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 document.write("Estamos en el año "+ fecha.getFullYear() + " y hoy es " + dia[fecha.getDay()] + " " +fecha.getDate() + " de " + mes[fecha.getMonth()]);
+//Calcula si eres mayor de edad
 let nac = window.prompt("Introduce tu año de nacimiento:");
 if((fecha.getFullYear-nac)>= 18){
 } else {
     document.write("No tiene la edad suficiente para votar");
 }
+//Comprueba si la contraseña ha caducado
