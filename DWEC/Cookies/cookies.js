ad898e4f0ba52d327b26d23edf8ab6c92ef3441b
adrialba <adrian.alba@outlook.es>
2022-11-03 20:13:33 +0100

    Subida

--- DWEC/Cookies/cookies.js
+++ DWEC/Cookies/cookies.js
@@ -1,15 +1,49 @@
-function setCookie(cname, cvalue, expdays) {
+function create(cname, cvalue, expdays) {
     let d = new Date();
     d.setTime(d.getTime() + (expdays*24*60*60*1000));
     let expires = "expires=" + d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
-function getCookies() {
-    let valor = window.prompt("Introduce el nombre a buscar");
-    document.cookie("Nombre=" + valor);
+function retreiveAll() {
+    /*let valor = window.prompt("Introduce el nombre a buscar");
+    document.cookie("Nombre=" + valor);*/
+    let cookies = document.cookie;
+    cookies.forEach(cookie => {
+        document.write(cookie);
+    });
 }
-
+function retrieve() {
+    let nombre = prompt("Introduce el nombre a buscar");
+    nombre+="=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(nombre) == 0) {
+        return c.substring(nombre.length, c.length);
+      }
+    }
+    return "";
+}
+function check() {
+    let nombre = retrieve("nombre");
+    if(nombre != ""){
+        alert("Bienvenido de nuevo" + nombre);
+    } else {
+        username = prompt("Intrduce tu nombre");
+        if(username != "" && username !=null){
+            create("nombre", username, 365);
+        }
+    }
+}
+function update() {
+    
+}
 function deleteCookie(cname) {
-    document.cookie(cname, )
+    let d = new Date();
+    document.cookie =  "Nombre=" + cname + "; expires=" + d + ";path=/"; 
 }
