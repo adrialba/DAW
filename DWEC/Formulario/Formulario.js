7b5947bd9b847f0928c69f21c2808a21395f519a
adrialba <adrian.alba@outlook.es>
2022-11-14 21:22:13 +0100

    Formulario

---
+++ DWEC/Formulario/Formulario.js
@@ -0,0 +1,16 @@
+function valNombre() {
+    
+}
+
+function valIdent() {
+    let ident = document.getElementsByName("ident").value;
+    switch (ident) {
+        case value:
+            
+            break;
+        case 
+        default:
+            break;
+    }
+
+}
